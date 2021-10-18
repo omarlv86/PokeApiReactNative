63fd8172f7c7dcc9d69b8af3563863f8dd72ab18
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-10-18 13:05:16 -0500

    main | Infinite scroll

--- src/hooks/usePokemonPaginate.tsx
+++ src/hooks/usePokemonPaginate.tsx
@@ -42,6 +42,7 @@ export const usePokemonPaginate = () => {
 
     return {
         isLoading,
-        simplePokemonList
+        simplePokemonList,
+        loadPokemons
     }
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { usePokemonPaginate } from '../hooks/usePokemonPaginate'
 import { styles } from '../theme/appTheme'
@@ -7,19 +7,48 @@ import { styles } from '../theme/appTheme'
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
-    const { simplePokemonList } = usePokemonPaginate();
-    console.log(simplePokemonList)
+    const { simplePokemonList, loadPokemons } = usePokemonPaginate();
+    //console.log(simplePokemonList)
     return (
-        <View>
+        <>
             <Image
               source={require('../assets/pokebola.png')}
               style={ styles.pokebolaBG}
             />
-            <Text style={{
+
+            <FlatList
+              data={ simplePokemonList }
+              keyExtractor={ (pokemon) => pokemon.id}
+              //renderItem={ ({ item }) => <Text style={{color:'black'}}>{ item.name }</Text>  }
+              showsVerticalScrollIndicator={false}
+              renderItem={ ({ item }) => (
+                <Image
+                  source={{ uri: item.picture }}
+                  style={{
+                      width:100,
+                      height:100
+                  }}
+                />
+              )}
+
+              //inifinite scroll
+              onEndReached={ loadPokemons } 
+              onEndReachedThreshold={ 0.4 }
+
+              ListFooterComponent={(
+                <ActivityIndicator 
+                  style={{height:100 }}
+                  size={20}
+                  color="grey"
+                />
+              )}
+            />
+            {/* <Text style={{
                 ...styles.title,
                 ...styles.globalMargin,
                 top: top + 20
-                }}>Pokedex</Text>
-        </View>
+                }}>Pokedex
+            </Text> */}
+        </>
     )
 }
